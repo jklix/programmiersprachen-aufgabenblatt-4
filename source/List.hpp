1911df37c05c878bbeda2895e232f166c15a824b
Judith Klix <judith.klix@uni-weimar.de>
2018-06-08 19:22:05 +0200

    Aufgabe 4.3

--- source/List.hpp
+++ source/List.hpp
@@ -2,6 +2,8 @@
 #define BUW_LIST_HPP
 
 #include <cstddef>
+#include <list>
+#include <iostream>
 
 //List.hpp
 
@@ -53,23 +55,136 @@ class List
 
 
     //Aufgabe 4.2
+    
+    //Default Konstruktor
+    
+    List():
+    size_{0},
+    first_{nullptr},
+    last_{nullptr}
+    {}
+    
+    bool empty() const
+    {
+        if(size() == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    std::size_t size() const
+    {
+        return size_;
+    }
 
-//Default Konstruktor
-List():
-size{0},
-first_{nullptr},
-last_{nullptr}
-{}
+    // Aufgabe 4.3
 
-bool empty() const
-{
-    return size_ == 0;
-}
+    T& front() const
+    {
+        if (empty())
+        {
+            std::cout << "Die Liste ist leer";
+        }
+        else
+        {
+            return first_ -> value;
+        }
+    }
 
-std::size_t size() const
-{
-    return size_;
-}
+    T& last() const
+    {
+        if (empty())
+        {
+            std::cout << "Die Liste ist leer";
+        }
+        else
+        {
+            return last_ -> value;
+        }
+    }
+
+
+    void push_front(T const& value)
+    {
+        ListNode <T>* node = new ListNode <T> {value, nullptr, nullptr};
+
+        if(!empty())
+        {
+            first_ -> prev = node;
+            node -> next = first_;
+        }
+        else
+        {
+            last_ = node;
+        }
+
+        first_ = node;
+        ++size_;
+    }
+
+    void push_back(T const& value)
+    {
+        ListNode <T>* node = new ListNode <T> {value, nullptr, nullptr};
+
+        if(!empty())
+        {
+            last_ -> next = node;
+            node -> prev = last_;
+        }
+        else
+        {
+            first_ = node;
+        }
+
+        last_ = node;
+        ++size_;
+    }
+
+    void pop_front()
+    {
+        if(size() == 1)
+        {
+            first_ = last_ = nullptr;
+            --size_;
+        }
+        else if(size() > 1)
+        {
+            first_ = (*first_).next;
+            --size_;
+        }
+        else
+        {
+            std::cout << "Liste ist bereits leer";
+        }
+    }
+
+    void pop_back()
+    {
+        if(size() == 1)
+        {
+            first_ = last_ = nullptr;
+            --size_;
+        }
+        else if(size() > 1)
+        {
+            last_ = (*last_).prev;
+            --size_;
+        }
+        else
+        {
+            std::cout << "Liste ist bereits leer";
+        }
+    }
+
+
+
+
+
+
 
 
 
---
+++ source/TestList.cpp
@@ -0,0 +1,96 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include "List.hpp"
+
+//Aufgabe 4.2
+
+TEST_CASE("size", "[listSize]")
+{
+    List <int> l1;
+    List <int> l2;
+
+    l2.push_back(1);
+    l2.push_back(2);
+    l2.push_back(3);
+
+    REQUIRE(l1.size() == 0);
+    REQUIRE(l2.size() == 3);
+
+    l2.pop_back();
+    REQUIRE (l2.size() == 2);
+
+    l2.pop_front();
+    REQUIRE (l2.size() == 1);
+}
+
+TEST_CASE("empty", "[list_empty]")
+{
+    List <int> l1;
+    List <int> l2;
+
+    l2.push_back(1);
+    l2.push_back(2);
+    l2.push_back(3);
+
+    REQUIRE(l1.empty() == true);
+    REQUIRE(l2.empty() == false);
+}
+
+
+// Aufgabe 4.3
+
+TEST_CASE("push_front", "[list_push_front]")
+{
+    List <int> l1;
+    l1.push_front(5);
+    l1.push_front(2);
+
+    REQUIRE(2 == l1.front());
+}
+
+TEST_CASE("push_back", "[list_push_back]")
+{
+    List <int> l1;
+    l1.push_back(2);
+    l1.push_back(6);
+
+    REQUIRE(6 == l1.last());
+}
+
+TEST_CASE("pop_front", "[list_pop_front]")
+{
+    List <int> l1;
+    l1.push_back(2);
+    l1.push_back(6);
+    l1.push_back(8);
+
+    l1.pop_front();
+
+    REQUIRE(6 == l1.front());
+
+    REQUIRE(l1.empty() == false);
+    l1.pop_front();
+    l1.pop_front();
+    REQUIRE(l1.empty() == true);
+
+}
+
+TEST_CASE("pop_back", "[list_pop_back]")
+{
+    List <int> l1;
+    l1.push_back(2);
+    l1.push_back(6);
+    l1.push_back(8);
+
+    l1.pop_back();
+
+    REQUIRE(6 == l1.last());
+}
+
+
+
+int main (int argc, char *argv[])
+{
+    return Catch::Session().run(argc, argv);
+}
+
